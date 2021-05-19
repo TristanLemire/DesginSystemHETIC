d73e8abc85b83aa28871e226fe9f72d286b94e7c
LimongiVirgil <limongi.virgil@gmail.com>
2021-05-19 15:48:29 +0200

    remove link from button component from storybook

--- src/components/organisms/LoginForm.js
+++ src/components/organisms/LoginForm.js
@@ -85,7 +85,6 @@ export const LoginForm = () => {
       </form>
       <div>Forgot password ?</div>
       <Button
-        isAction={true}
         text="Sign in"
         isDisabled={isSubmitDisabled}
         handleClick={handleFormSubmit}
