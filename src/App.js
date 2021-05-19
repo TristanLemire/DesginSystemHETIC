bfad511e1d77d47266b479ab290566bf565ff2d5
TristanLemire <tristan.lemire.sio@gmail.com>
2021-05-19 18:27:27 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import styled from "styled-components";
 import { Header } from "./components/organisms/Header";
 import { LoginPage } from "./components/pages/LoginPage";
 import "./styles/vendors/reset.css";
-import { Text } from "./components/atoms/Text";
 
 export const App = () => {
   return (
