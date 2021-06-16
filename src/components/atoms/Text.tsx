0d0462ed753b01b43dac2d83855c1f02b5ac2559
TristanLemire <tristan.lemire.sio@gmail.com>
2021-06-16 10:12:15 +0200

    fix input Text

--- src/components/atoms/Text.tsx
+++ src/components/atoms/Text.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import { fontStyleType, space } from "../../styles/const";
 
 type TextProps = {
-  tag: "h1" | "h2" | "h3" | "h4" | "span" | "p";
   type: fontStyleType;
   color: string;
   children: string;
@@ -11,18 +10,16 @@ type TextProps = {
 };
 
 export const Text = (props: TextProps) => {
-  const CustomTag = `${props.tag}` as keyof JSX.IntrinsicElements;
-
-  const CustomTagStyled = styled(CustomTag)`
-    color: ${props.color};
-    font-size: ${props.type.fontSize};
-    font-weight: ${props.type.fontWeight};
-    letter-spacing: ${props.type.letterSpacing};
-    line-height: ${props.type.lineHeight};
-    font-style: ${props.type.fontStyle};
-    border-bottom: ${props.underline && `solid ${props.color}`};
-    padding-bottom:  ${props.underline && space.xxxs};
-  `;
-
-  return <CustomTagStyled>{props.children}</CustomTagStyled>;
+  return <CustomTagStyled theme={props}>{props.children}</CustomTagStyled>;
 };
+
+const CustomTagStyled = styled.p`
+  color: ${(props) => props.theme.color};
+  font-size: ${(props) => props.theme.type.fontSize};
+  font-weight: ${(props) => props.theme.type.fontWeight};
+  letter-spacing: ${(props) => props.theme.type.letterSpacing};
+  line-height: ${(props) => props.theme.type.lineHeight};
+  font-style: ${(props) => props.theme.type.fontStyle};
+  border-bottom: ${(props) => props.theme.underline && `solid ${props.color}`};
+  padding-bottom: ${(props) => props.theme.underline && space.xxxs};
+`;
